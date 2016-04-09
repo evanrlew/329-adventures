2d99972a7516f606707b7ec98a14a0eeb79af467
Randy Fung <rafffung@gmail.com>
2016-04-08 17:27:33 -0700

    90% credit
    Changes right before starting the game

--- proj1_LCD/proj1_LCD/proj1_LCD/proj1_LCD.c
+++ proj1_LCD/proj1_LCD/proj1_LCD/proj1_LCD.c
@@ -1,47 +1,37 @@
 #include <avr/io.h>    	// registers locations and some other things
 #define F_CPU 16000000UL  	// tells compiler the freq of your processor
 #include <util/delay.h>    // software delay functions
+#include <avr/interrupt.h>
+
+static char btn_cnt = 0;
 
 void lcd_wr_cmd(uint8_t input) {
-	PORTB = 0b00000100; // E = 1
+	PORTB |= 0b00000100; // E = 1
 	PORTD = input;
 	_delay_us(1);
-	PORTB = 0b00000000; // E = 0
+	PORTB &= ~(1 << 2); // E = 0
 }
 
 void lcd_wr_data(uint8_t input) {
-	PORTB = 0b00000001; // RS = 1
+	PORTB |= 0b00000001; // RS = 1
 	_delay_us(1);
 	
 	PORTD = input;
 	_delay_us(1);
 	
-	PORTB = 0b00000101; // RS = 1, E = 1
+	PORTB |= 0b00000101; // RS = 1, E = 1
 	_delay_us(1);
 	
 	//PORTB = 0b00000001; // RS = 1, E = 0
 	//_delay_us(1);
 	
-	PORTB = 0b00000000; // RS = 0, E = 0
-	_delay_us(1);
+	PORTB &= ~(5); // RS = 0, E = 0
+	_delay_us(100); // Delay after each write
 }
 
 void lcd_write_string(char* inputString) {
 	while(*inputString) {
-		switch(*inputString) {
-			case 'A': lcd_wr_data(0b01000001); break;
-			case 'B': lcd_wr_data(0b01000010); break;
-			case 'C': lcd_wr_data(0b01000011); break;
-			case 'D': lcd_wr_data(0b01000100); break;
-			case 'E': lcd_wr_data(0b01000101); break;
-			case 'H': lcd_wr_data(0b01001000); break;
-			case 'L': lcd_wr_data(0b01001100); break;
-			case 'O': lcd_wr_data(0b01001111); break;
-			case '!': lcd_wr_data(0b00100001); break;			
-	
-			default: lcd_wr_data(0b1111111); break;
-		}
-		_delay_us(100); // Delay after each write
+		lcd_wr_data(*inputString);
 		inputString++;
 	}
 }
@@ -50,12 +40,21 @@ int main(void) {
 	DDRD = 0xFF;
 	DDRB = 0b00000111;
 	
+	PORTB |= 1 << PB3;
+	
+	// setup interupts
+	PCICR = 0x01;   // enable PCINT7..0
+	PCMSK0 = 0x03;  // enable PCINT3 interrupt
+	PCIFR = 0x01;   // clear previous interrupts
+	
+	sei();
+	
 	_delay_ms(40);
 
 	lcd_wr_cmd(0b00111000);
 	_delay_us(50);
 	
-	lcd_wr_cmd(0b00001111); // turn on display
+	lcd_wr_cmd(0b00001100); // turn on display
 	_delay_us(50);
 	
 	lcd_wr_cmd(0b00000001); // clear display
@@ -63,17 +62,31 @@ int main(void) {
 	
 	lcd_wr_cmd(0b00000110); // set entry mode
 	_delay_ms(1);
-	
-	//lcd_wr_cmd(0b10000000);
-	//lcd_wr_cmd(0b00011100); // move the cursor thing
-		
-	lcd_wr_cmd(0b10000000);
-	_delay_us(100);
-	
-//	lcd_wr_data(0x41);
-	
-	lcd_write_string("HELLO!");
-	while(2);
+			
+//	lcd_write_string("Hello World!");
 
+	lcd_wr_cmd(0b10000000 | 0x45);
+	_delay_us(50);
+	lcd_wr_data('L');
+	
+	lcd_wr_cmd(0b10000000 | 0x0A);
+	_delay_us(50);
+	lcd_wr_data('b');
+
+	while (2) {
+		if (btn_cnt == 1) {
+//		if (PINB ^ 1 << PB3) {
+			lcd_wr_cmd(0b00000001); // clear display
+			_delay_ms(5);
+			
+			lcd_write_string("dA king");
+		}
+	}
+	
 	return 1;
+}
+
+ISR(PCINT3_vect) {
+	btn_cnt++;
+	_delay_us(500);
 }
